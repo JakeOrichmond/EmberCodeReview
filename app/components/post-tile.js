a5db502f1edfd14c11dca42c099a6eefd622d64f
Joshua Logan <joshlogan32323@gmail.com>
2016-06-14 13:00:20 -0700

    added update functionality

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateRentalForm: false,
   actions: {
     delete(post) {
     if (confirm('Are you sure you want to delete this post?')) {
       this.sendAction('destroyPost', post);
     }
-  }
+  },
+  update(rental, params) {
+    this.sendAction('update', rental, params);
+  },
 }
 });
