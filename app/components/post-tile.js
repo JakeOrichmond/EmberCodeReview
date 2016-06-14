4acca5476f2b54e94121d4ab15130af0d49a27d0
Joshua Logan <joshlogan32323@gmail.com>
2016-06-14 13:19:57 -0700

    questions now have a detail page to display full details of the question

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -3,13 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   updateRentalForm: false,
   actions: {
-    delete(post) {
-    if (confirm('Are you sure you want to delete this post?')) {
-      this.sendAction('destroyPost', post);
-    }
-  },
-  update(rental, params) {
-    this.sendAction('update', rental, params);
-  },
-}
+    update(rental, params) {
+      this.sendAction('update', rental, params);
+    },
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -9,21 +9,6 @@ export default Ember.Route.extend({
       var newPost = this.store.createRecord('post', params);
       newPost.save();
       this.transitionTo('index');
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-
-    destroyPost(post) {
-      post.destroyRecord();
-      this.transitionTo('index');
-    },
+    }
   }
 });
