d6adb81c9a8a7ce02a005249ce4c111c31a08e0a
Joshua Logan <joshlogan32323@gmail.com>
2016-06-14 13:44:29 -0700

    added materialize

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,8 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
-
+  app.import('bower_components/Materialize/dist/css/materialize.css');
+  app.import('bower_components/Materialize/dist/js/materialize.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
