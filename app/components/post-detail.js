29e6ff315d9791cce762ecf411c8649e6aeeba56
Joshua Logan <joshlogan32323@gmail.com>
2016-06-19 18:58:08 -0700

    added favorite-posts service topost-detail page

--- app/components/post-detail.js
+++ app/components/post-detail.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoritePosts: Ember.inject.service(),
   actions: {
     delete(post) {
       if (confirm('Are you sure you want to delete this post?')) {
         this.sendAction('destroyPost', post);
       }
+    },
+    addToFavorites(favorite) {
+      Ember.Logger.log(favorite);
+      this.get('favoritePosts').add(favorite);
     }
   }
 });
