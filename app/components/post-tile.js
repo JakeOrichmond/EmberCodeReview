c2d19a4a95c09277d73f81ba57f5ed1101c68a31
Joshua Logan <joshlogan32323@gmail.com>
2016-06-22 08:56:02 -0700

    added a computed property that displays how many answers a question has

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -3,13 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   favoritePosts: Ember.inject.service(),
   updateRentalForm: false,
+  answerTally: Ember.computed('post.author','post.answers.length', function() {
+    return this.get('post.author') + '\'s' + ' question has ' + this.get('post.answers.length') + ' answers.';
+  }),
   actions: {
-    update(rental, params) {
-      this.sendAction('update', rental, params);
+    update(post, params) {
+      this.sendAction('update', post, params);
     },
     addToFavorites(favorite) {
       Ember.Logger.log(favorite);
       this.get('favoritePosts').add(favorite);
     }
   }
+
+
 });
